ae330f61aaf551e2de6df7b8d34aa4bbd4d0e866
wolfep <wolfep@16.99.148.220>
2017-10-09 00:50:47 -0400

    Fix launch script so it can be executed from anywhere

--- rfid_reader/rfid_reader.ps1
+++ rfid_reader/rfid_reader.ps1
@@ -6,15 +6,34 @@
 # Description:
 #	Set up environment to run the mighty FRC2590 RFID reader program
 
-$current_dir = Get-Location
-#write-host "$current_dir"
+# PSScriptRoot is the directory from which we are executing
+# (where this script lives! the top level of our 
+# installation tree  - not the process's PWD)
+# However, we look for all input data files relative to 
+# our installation directy so we need to set and restore
+# the user's PWD
+$starting_dir = Get-Location 
 
-# The directory structure for the app is:
-#     rfid_reader
-#      bin/rfid_reader
-#      data
-#      lib
-# The required java command line is:
-#     java -classpath "bin;lib\*" rfid_reader.RFIDreader
 
-java -classpath "bin;lib\*" rfid_reader.RFIDreader
+try 
+{
+    # The directory structure for the app is:
+    #     rfid_reader
+    #      bin/rfid_reader
+    #      data
+    #      lib
+    # The required java command line is:
+    #     java -classpath "bin;lib\*" rfid_reader.RFIDreader
+
+    $cp = $PSScriptRoot + "\bin;" + $PSScriptRoot + "\lib\*"
+
+    Set-location $PSScriptRoot  # cd to execution dir
+
+    #java -classpath "bin;lib\*" rfid_reader.RFIDreader
+    java -classpath $cp rfid_reader.RFIDreader
+}
+finally
+{
+    # write-host "here: $current_dir"
+    Set-Location $starting_dir
+}
